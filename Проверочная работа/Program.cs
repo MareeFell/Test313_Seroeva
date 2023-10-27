46a31628deef03a3d13cdfb1dabe6d238998b99b
MareeC <seroeva.marinaa2015@yandex.ru>
2023-10-27 10:34:29 +0300

    dlina

--- Проверочная работа/Program.cs
+++ Проверочная работа/Program.cs
@@ -1,4 +1,4 @@
-﻿double x1, x2, y1, y2;
+﻿double x1, x2, y1, y2, dlin;
 Random rand = new Random();
 Console.WriteLine("Сгенерировать случайные точки? 1-Да; 2-Нет");
 string ans = Console.ReadLine();
@@ -8,8 +8,10 @@ if (ans == "1")
     y1 = rand.NextDouble()*100;
     x2= rand.NextDouble()*100;
     y2= rand.NextDouble()*100;
-    Console.WriteLine("Координаты первой точки: " + "x1= " + Convert.ToString(x1) + "\n y1= "+ Convert.ToString(y1));
-    Console.WriteLine("Координаты второй точки: " + "x2= " + Convert.ToString(x2) + "\n y2= " + Convert.ToString(y2));
-    Console.ReadKey();
+    Console.WriteLine("Координаты первой точки: " + "\n x1= " + Convert.ToString(x1) + "\n y1= "+ Convert.ToString(y1));
+    Console.WriteLine("Координаты второй точки: " + "\n x2= " + Convert.ToString(x2) + "\n y2= " + Convert.ToString(y2));
+    dlin = Math.Sqrt(((x1-y1)*(x1-y1)) + ((x2-y2)*(x2-y2)));
+    Console.WriteLine("Длинна отрезка: " + Convert.ToString(dlin));
 }
+Console.ReadKey();
 
